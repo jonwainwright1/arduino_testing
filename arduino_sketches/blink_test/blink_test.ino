ced4e1130a06e38e5bc26d0c8c00c2dc75ee54a8
Crackpot Gumption <crackpot.gumption@gmail.com>
2024-11-12 22:48:25 -0500

    Adding iterative lights test

--- arduino_sketches/blink_test/blink_test.ino
+++ arduino_sketches/blink_test/blink_test.ino
@@ -1,13 +1,45 @@
+#include "Arduino_LED_Matrix.h"
+
+
+ArduinoLEDMatrix matrix;
+int y = 0;
+int x = 0;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
+  matrix.begin();
 }
 
 void loop() {
+  delay(50);
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  byte frame[8][12] = {
+    {0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0}
+  };
 
+
+  frame[x][y] = 0;
+
+  x += 1;
+
+  if (x > 7) {
+    x = 0;
+    y += 1;
+  }
+
+  if (y > 11) {
+    y = 0;
+  }
+
+  frame[x][y] = 1;
+
+  matrix.renderBitmap(frame, 8, 12);
 }
